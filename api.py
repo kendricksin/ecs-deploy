eacd3e5f7af45a4f2488e245be86577caefaedf1
unknown <kendrick_sin@hotmail.com>
2024-07-06 17:06:24 +0800

    changed api pointer

--- api.py
+++ api.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     parser.add_argument("--port", type=int, default=8000, help="Port to run the server on")
     args = parser.parse_args()
 
-    uvicorn.run(app, host=args.host, port=args.port)
+    uvicorn.run(flip, host=args.host, port=args.port)
